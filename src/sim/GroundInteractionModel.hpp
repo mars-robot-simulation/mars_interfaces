c2683bb5fe93ec22d9e1db5d69326b219ae44254
Julian Liersch <julian.liersch@dfki.de>
2024-06-14 16:17:11 +0200

    GroundInteractionInterface: Enabled informing gim about being used in a contact plugin

--- src/sim/GroundInteractionModel.hpp
+++ src/sim/GroundInteractionModel.hpp
@@ -6,11 +6,14 @@ namespace mars
 {
     namespace interfaces
     {
+        class ContactPluginInterface;
+
         class GroundInteractionModel
         {
         public:
             virtual void update(interfaces::ContactData& contactData) = 0;
             virtual void prePhysicsUpdate() = 0;
+            virtual void registerUsingContactPlugin(const ContactPluginInterface* const contactPlugin) {}
         };
     }
 }

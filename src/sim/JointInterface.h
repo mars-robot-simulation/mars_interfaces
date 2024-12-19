dab3184cbbc3b1b7e81f0a0ce82ce7b745834b75
Malte Langosz <malte.langosz@dfki.de>
2024-12-19 12:17:14 +0100

    Add interface for joint friction coefficients

--- src/sim/JointInterface.h
+++ src/sim/JointInterface.h
@@ -61,6 +61,8 @@ namespace mars
             virtual void setLowStop2(sReal lowStop) = 0;
             virtual void setHighStop2(sReal lowStop) = 0;
             virtual void setCFM(sReal cfm) = 0;
+            virtual void setJointFrictionCoefficient(sReal friction) = 0;
+            virtual sReal getJointFrictionCoefficient() = 0;
             virtual void getDataBrokerNames(std::string& groupName, std::string& dataName) = 0;
 
         };

e39be9e0d4449bf7e725f9023427e6ab46a018e5
Malte Langosz <malte.langosz@dfki.de>
2026-03-24 09:53:09 +0100

    Add scale callbacks to manipulator client

--- src/graphics/ManipulatorClient.hpp
+++ src/graphics/ManipulatorClient.hpp
@@ -12,10 +12,13 @@ namespace mars
 
             virtual void moveX(double value) = 0;
             virtual void moveY(double value) = 0;
-            virtual void moveZ(double value) = 0;            
-            virtual void rotX(double value) = 0;            
-            virtual void rotY(double value) = 0;            
-            virtual void rotZ(double value) = 0;            
+            virtual void moveZ(double value) = 0;
+            virtual void rotX(double value) = 0;
+            virtual void rotY(double value) = 0;
+            virtual void rotZ(double value) = 0;
+            virtual void scaleX(double value) = 0;
+            virtual void scaleY(double value) = 0;
+            virtual void scaleZ(double value) = 0;
             virtual bool keyPress(char c) = 0;
         };
     }
